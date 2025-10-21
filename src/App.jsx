04d9291ef46cd8e050e355c246290b8a25708c9d
MAngelCar0 <miguelacaro0222@gmail.com>
2025-10-21 11:34:29 -0500

    Se creo el incio de sesion y el registro, cada uno enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterApp } from './routes/RouterApp';
 
 function App() {
  
   return (
-    <section>
-      
-    </section>
+    <RouterProvider router={createBrowserRouter(RouterApp)} />
   )
 }
-
 export default App
